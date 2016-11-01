ad7f3b8ae51c707998b0afbf71d6506d02abf3b4
root <mikehike123@gmail.com>
2016-10-31 21:01:10 -0700

    fixed route file

--- routes/web.php
+++ routes/web.php
@@ -15,11 +15,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('search', function () {
-    return view('Instructors.search.index');
-});
-
-
 Route::get('admin', 'Admin\\AdminController@index');
 Route::get('admin/give-role-permissions', 'Admin\\AdminController@getGiveRolePermissions');
 Route::post('admin/give-role-permissions', 'Admin\\AdminController@postGiveRolePermissions');
@@ -31,13 +26,12 @@ Auth::routes();
 Route::get('/home', 'HomeController@index');
 
 Route::resource('Instructors/swimmers', 'Instructors\\SwimmersController');
-
 Route::resource('proficiency-levels', 'SwimAdmin\\ProficiencyLevelsController');
 
-Route::resource('admin/skill/store', 'SwimAdmin\\SkillController@store');
-Route::resource('admin/skill/{card_id}/showAll', 'SwimAdmin\\SkillController@showAll');
-Route::resource('admin/skill/{card_id}/create', 'SwimAdmin\\SkillController@create');
-Route::resource('admin/skill/{id}/delete', 'SwimAdmin\\SkillController@destroy');
-Route::resource('admin/skill/{id}/edit', 'SwimAdmin\\SkillController@edit');
-Route::resource('admin/skill/{id}/update', 'SwimAdmin\\SkillController@update');
+Route::get('admin/skill/store', 'SwimAdmin\\SkillController@store');
+Route::get('admin/skill/{card_id}/showAll', 'SwimAdmin\\SkillController@showAll');
+Route::get('admin/skill/{card_id}/create', 'SwimAdmin\\SkillController@create');
+Route::get('admin/skill/{id}/delete', 'SwimAdmin\\SkillController@destroy');
+Route::get('admin/skill/{id}/edit', 'SwimAdmin\\SkillController@edit');
+Route::get('admin/skill/{id}/update', 'SwimAdmin\\SkillController@update');
 Route::resource('admin/skill-cards', 'SwimAdmin\\SkillCardsController');
