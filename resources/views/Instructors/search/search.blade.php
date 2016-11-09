aa1520f2f20b4d5ddcf3afa06e3dd3088a0d51b3
mikehike123 <mikehike123@gmail.com>
2016-11-08 18:18:50 -0800

    formatting of search box

--- resources/views/Instructors/search/search.blade.php
+++ resources/views/Instructors/search/search.blade.php
@@ -45,14 +45,17 @@
         
         <div class="col-sm-10 col-sm-offset-1 col-xs-12 " style="margin-top:20px;margin-bottom:20px;">
             <div class="control-group">
-                {!! Form::open(['url' => '/search/lastNamesLike','method' => 'get', 'class' => 'form-horizontal', 'files' => true]) !!}    
-                    {!! Form::label('last_name', 'Last Name:', ['class' => 'col-sm-3 control-label']) !!}
-                        <div class="col-sm-5" style="padding-right:1">
-                            {!! Form::text('last_name', null, ['class' => 'form-control search', 'required' => 'required']) !!}
-                            
-                        </div>
-                    
-                    <button type="submit" class="btn btn-success col-sm-1" style="">Search</button>
+                {!! Form::open(['url' => '/search/lastNamesLike','method' => 'get', 'class' => 'form-horizontal', 'files' => true]) !!}
+                    <div class="col-sm-2  " style="padding-left:12px;padding-top:5px;">
+                    {!! Form::label('last_name', 'Last Name:', ['class' => 'control  ']) !!}
+                    </div>
+                    <div class="col-sm-8  " style="margin-bottom:5px;">   
+                    {!! Form::text('last_name', null, ['class' => ' form-control search ', 'required' => 'required']) !!}
+                    </div>       
+                        
+                    <div class="col-sm-2 " style="">
+                    <button type="submit" class="btn btn-success " style="">Search</button>
+                    </div>
                  {!! Form::close() !!}
         </div>
         </div>
